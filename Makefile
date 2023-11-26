a14bd5a9c7be6b3369cd88207e9fe28c40e3a377
Carlos Astorga <castorga@car14s6.42barcelona.com>
2023-11-26 16:53:27 +0100

    makefile and structure

---
+++ Makefile
@@ -0,0 +1,53 @@
+NAME = so_long
+
+LIBFT_DIR = libft/
+MLX_DIR = mlx/
+LIBFT_FILE = libft.a
+MLX_FILE = libmlx.a
+LIBFT = $(addprefix $(LIBFT_DIR), $(LIBFT_FILE))
+MLX = $(addprefix $(MLX_DIR), $(MLX_FILE))
+
+SRCS_DIR = src/
+SRC_FILES =	 main.c
+
+OBJS_DIR =	objs/
+OBJ_FILES = $(SRC_FILES:.c=.o)
+OBJS = $(addprefix $(OBJS_DIR), $(OBJ_FILES))
+DEPS        := $(OBJS:.o=.d)
+CC = clang -g
+CFLAGS = -Wall -Wextra -Werror
+MLXFLAGS = -Imlx -Lmlx -lmlx -framework OpenGL -framework AppKit
+RM = rm -rf
+AR = ar rc
+#INCLUDE  = -I include/ -I libft/ mlx/
+INCLUDE = -I include/ -I libft/ -I mlx/
+
+
+all: 		$(NAME)
+
+subsystems:
+			@make -C $(LIBFT_DIR)
+			@make -C $(MLX_DIR)
+
+$(NAME):	subsystems $(OBJS_DIR) $(OBJS)
+			@$(CC) $(CFLAGS) $(OBJS) -L libft/ -lft $(MLXFLAGS) -o $@
+
+$(OBJS_DIR):
+						@mkdir $@
+
+$(OBJS_DIR)%.o:	$(SRCS_DIR)%.c
+				$(CC) $(CFLAGS) -c $< -o $@ $(INCLUDE)
+
+clean: 		
+			make -C $(LIBFT_DIR) clean
+			@$(RM) $(OBJS) $(DEPS)
+
+
+fclean: 	clean
+			make -C $(LIBFT_DIR) fclean
+			@$(RM) $(NAME)
+
+re:			fclean all
+-include $(DEPS)			
+
+.PHONY: all clean fclean re subsystems
---
+++ src/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: castorga <castorga@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/26 15:46:30 by castorga          #+#    #+#             */
+/*   Updated: 2023/11/26 15:46:32 by castorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mlx/mlx.h"
+
+/*
+cc main.c -Imlx -Lmlx -lmlx -framework OpenGL -framework AppKit
+*/
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
